6f544dbf9b55a8ba6a2c4e4d72040ba375e34aaf
Cedric Segura <cedricsegurafr@gmail.com>
2022-06-26 21:33:08 +0200

    Home setUp & css fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Footer from './components/Footer';
 import Header from './components/Header';
+import Menu from './components/Menu';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import './style/App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Container className='main'>
+          <Menu/>
+          <Home/>
+        </Container>
       <Footer/>
     </div>
   );
---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import '../style/Menu.css';
+
+
+
+export default class Menu extends React.Component {
+    render() {
+        return (
+            <>
+                <Container className="menu">
+                </Container>
+            </>
+        );
+    }
+
+}
