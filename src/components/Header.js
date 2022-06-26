4e59c5f977cc0e9c3c1622e0d1b1956ef796c43b
Cedric Segura <cedricsegurafr@gmail.com>
2022-06-26 13:47:59 +0200

    header setUp with search input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,17 @@
 import React from "react";
-import { Container, Navbar, Nav, Form } from "react-bootstrap";
+import { Container, Navbar, Form } from "react-bootstrap";
+import '../style/Header.css';
 
 export default class Header extends React.Component {
     render() {
         return (
             <>
                 <Navbar bg="primary" variant="dark" sticky="top">
-                    <Container>
-                        <Navbar.Brand href="#home">MyApp</Navbar.Brand>
-                            <Nav className="me-auto">
-                            {
-                                /*
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#home2">Features</Nav.Link>
-                                <Nav.Link href="#home3">Pricing</Nav.Link>
-                                */
-                            }
-
+                    <Container className="content">
+                        <Container className="info">
+                            <Navbar.Brand href="#home">MyApp</Navbar.Brand>
+                        </Container>
+                        <Container className="search">
                             <Form>
                                 <Form.Control
                                     type="search"
@@ -25,8 +20,7 @@ export default class Header extends React.Component {
                                     aria-label="Search"
                                 />
                             </Form>                     
-                            
-                        </Nav>
+                        </Container>
                     </Container>
                 </Navbar>
             </>
