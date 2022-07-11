7012912959371a0103815f641bfb782be013ce54
Cedric Segura <cedricsegurafr@gmail.com>
2022-07-11 20:04:31 +0200

    Home page update

--- frontEnd/src/pages/Home.js
+++ frontEnd/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Table } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 import '../style/Home.css';
 
 export default class Menu extends React.Component {
@@ -7,35 +7,15 @@ export default class Menu extends React.Component {
         return (
             <>
                 <Container className="home">
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                    </Table>
+                    <Form style={{position:'fixed', display: 'inline-grid', top: '25%', left: '40%', width: '500px'}}>
+                        <Form.Group style={{paddingBottom:'30px'}}>
+                            <Form.Label>Enter your message</Form.Label>
+                            <Form.Control type="text"/>
+                        </Form.Group>
+                        <Button type="submit">
+                            Submit
+                        </Button>
+                    </Form>
                 </Container>
             </>
         );
