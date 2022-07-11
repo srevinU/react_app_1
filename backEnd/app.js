fa13cd9c9b7447dacb7baca374a7de85194f4f71
Cedric Segura <cedricsegurafr@gmail.com>
2022-07-11 21:46:51 +0200

    Axios clean and express setUp

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,1 +1,13 @@
-console.log('Test');
+const app = require('express')();
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    () => console.log(`It's alive on http://localhost:${PORT}`)
+);
+
+app.get('/test', (req, res) => {
+    res.status(200).send({
+        test: 'myFirstTest'
+    })
+});
